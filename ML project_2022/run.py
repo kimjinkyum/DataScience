a30f79967173798ce466bad6abd554e9c79faf79
kimjinkyum <kimo1113@daum.net>
2022-06-27 21:17:54 +0900

    [Implementation] Data Preprocessing

--- ML project_2022/run.py
+++ ML project_2022/run.py
@@ -37,6 +37,25 @@ def reduce_mem_usage(df, verbose=True):
     return df
 
 
+def change_hour(x):
+    return x["Timestamp"].split()[1].split(":")[0]
+
+def change_ip_class(x):
+    parts = x.split(".")
+    one_gram = int(parts[0])
+    if 0 <= one_gram <= 127:
+        class_ip = "A"
+    elif 128 <= one_gram <= 191:
+        class_ip = "B"
+    elif 192 <= one_gram <= 223:
+        class_ip = "C"
+    elif 224 <= one_gram <= 239:
+        class_ip = "D"
+    else:
+        class_ip = "E"
+    return class_ip
+
+
 if __name__ == "__main__":
     # Get data
     df = pd.read_csv("data/Darknet.csv", error_bad_lines=False)
@@ -50,3 +69,29 @@ if __name__ == "__main__":
     sns.histplot(data=df, x="Label.1", stat="probability")
     plt.xlabel('')
     plt.show()
+
+    # 1) Column 한개만 가지고 있는 column 삭제
+    unique_column = []
+    for k in df.columns:
+        if len(df[k].unique()) == 1:
+            # print("Column name:", k, df[k].unique())
+            unique_column.append(k)
+
+    df = df.drop(unique_column, axis=1)
+
+    # 2) Timestamp -> 시간
+    df["hour"] = df.apply(change_hour, axis=1).astype("int")
+
+    # 3) Label.1 중복 값 대처
+    df["Label.1"].loc[df["Label.1"] == "AUDIO-STREAMING"] = "Audio-Streaming"
+    df["Label.1"].loc[df["Label.1"] == "File-transfer"] = "File-Transfer"
+    df["Label.1"].loc[df["Label.1"] == "Video-streaming"] = "Video-Streaming"
+
+    # 4) IP 값을 -> IP Class 값으로 (A, B, C, D, E class)
+    df["Src IP class"] = df["Src IP"].apply(change_ip_class)
+    df["Dst IP class"] = df["Dst IP"].apply(change_ip_class)
+    
+    # 5) 사용하지 않은 column 삭제
+    df = df.drop(
+        ["Flow ID", "Timestamp", "Src IP", "Dst IP", "Flow Duration", "Flow Bytes/s", "Flow Packets/s", "Timestamp"],
+        axis=1)
